69dd69306bb7fde1015e8d8b630cba208ac9411a
aangelovv <angelangellov95@gmail.com>
2023-03-21 20:31:41 +0200

    Add Home component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,39 @@
+import * as projectService from "../../services/projectService";
+import { useEffect, useState } from "react";
+import styles from "./Home.module.css";
+import HomeCard from "./HomeCard";
+import OfficeCard from "./OfficeCard";
+
 const Home = () => {
-  return <p>This is Home page</p>;
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    projectService.getLatest().then((result) => setProjects(result));
+  }, []);
+
+  return (
+    <>
+      <article className={styles.background}>
+        <img
+          src="https://c4.wallpaperflare.com/wallpaper/111/745/193/reactjs-javascript-programming-programming-language-hd-wallpaper-preview.jpg"
+          alt="react-pic"
+          className={styles.image}
+        />
+      </article>
+      <h2 className={styles["projects-collection-heading"]}>
+        Projects collection
+      </h2>
+      <section className={styles["projects-collection"]}>
+        {projects.length > 0 ? (
+          projects.map((x) => <HomeCard key={x.id} project={x} />)
+        ) : (
+          <h3 className={styles["no-projects"]}>There are no projects.</h3>
+        )}
+      </section>
+      <h2 className={styles["offices-heading"]}>Offices Locations</h2>
+      <section className={styles.offices}>{<OfficeCard />}</section>
+    </>
+  );
 };
 
 export default Home;
